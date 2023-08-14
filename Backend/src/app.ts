bd3007ddd4234ad01c7a6e2d89353d7248aea267
Anirudh Parida <86073238+AnirudhParida@users.noreply.github.com>
2023-08-14 20:57:00 +0530

    error handling

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import noteRoutes from './routes/notes';
 import morgan from 'morgan';
+import createHttpError,{isHttpError} from 'http-errors';
 
 const app = express();
 
@@ -12,16 +13,19 @@ app.use(express.json());
 app.use("/api/notes", noteRoutes);
 
 app.use((req, res, next) => {
-    next(Error('Not found'));
+    next(createHttpError(404,'Not found'));
 })
 
 app.use((error:unknown, req:express.Request, res:express.Response, next:express.NextFunction) => {
     
         console.error(error);
-        if (error instanceof Error){
-            res.status(500).json({ error: error.message });
+        let errorMessage = "An error occurred, please try again later";
+        let statusCode = 500;
+        if(isHttpError(error)){
+            statusCode = error.status;
+            errorMessage = error.message;
         }
-
+        res.status(statusCode).json({error: errorMessage});
     next();
 });
 
